94ce3e6e3f923c0ac7d652bfceb641d934517ce9
송 필 수 <104068583+P1su@users.noreply.github.com>
2025-03-19 21:05:21 +0900

    Update 전화번호 목록.js

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,12 +1,11 @@
 function solution(phone_book) {
-  phone_book.sort(); // 전화번호 리스트를 정렬합니다.
-
+  phone_book.sort();//sort를 하면 가장 유사한 접두어가 옆에 오게 됨. 문자 기준 정렬!
+    
   for (let i = 0; i < phone_book.length - 1; i++) {
-    // 현재 번호와 다음 번호의 시작 부분이 같으면 접두어이므로 false를 반환합니다.
     if (phone_book[i] === phone_book[i + 1].substring(0, phone_book[i].length)) {
       return false;
     }
   }
 
-  return true; // 접두어가 없는 경우 true를 반환합니다.
+  return true;
 }
