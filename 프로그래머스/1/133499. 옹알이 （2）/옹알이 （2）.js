2367a76879c7140c1056a0f0fc7fe9c795147e8b
송 필 수 <104068583+P1su@users.noreply.github.com>
2025-03-31 11:03:39 +0900

    [level 1] Title: 옹알이 (2), Time: 0.54 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -8,15 +8,18 @@ function solution(babblings) {
     function slice(babbling, prevWord) {
         for(i = 0 ; i < 4 ; i++){
             if(babbling.startsWith(words[i])){
+                //이전 글자와 겹치면 return 
                 if(words[i] === prevWord){
                     return;
                 }else{
                     //문자열 자르기 substr
                     babbling = babbling.substr(words[i].length);
                     if(babbling === ''){
+                        //전부 발음
                         answer++;
                         return
                     }else{
+                        //남은 문자를 확인
                         slice(babbling, words[i]);
                     }
                 }
