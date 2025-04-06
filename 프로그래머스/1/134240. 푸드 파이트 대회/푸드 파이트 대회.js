8b91dffd27b436fe7253fdfaf0b76a2c2ef72f48
송 필 수 <104068583+P1su@users.noreply.github.com>
2025-04-06 13:59:44 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.39 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,16 @@
+function solution(food) {
+    
+    var answer = '';
+    
+    for(i = 1 ; i < food.length ; i++) {
+        //repeat(n) => 해당 문자열을 n번 반복
+        answer += String(i).repeat(food[i]/2);
+    }
+    //split() => 문자열을 배열로
+    //reverse() => 배열 뒤집기
+    //join() => 배열을 문자열로 합치기
+    var tmp = answer.split('').reverse().join('')
+    answer = answer + '0' + tmp;
+    
+    return answer;
+}
