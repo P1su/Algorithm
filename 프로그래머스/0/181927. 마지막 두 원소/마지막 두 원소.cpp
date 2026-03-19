e65e051469526cc2d5b17c78bc5aaf991ea6e8c4
송 필 수 <104068583+P1su@users.noreply.github.com>
2026-03-19 15:37:05 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    int last_idx = num_list.size() - 1;
+    if(num_list[last_idx] > num_list[last_idx - 1]){
+        num_list.push_back(num_list[last_idx] - num_list[last_idx - 1]);
+    }else{
+        num_list.push_back(num_list[last_idx] * 2);
+    }
+    return num_list;
+}
