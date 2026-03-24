2b76380788dee0f4d8fbf313edcf644efe673bf8
송 필 수 <104068583+P1su@users.noreply.github.com>
2026-03-24 12:28:32 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list, int n) {
+    int answer = 0;
+    for(auto num: num_list){
+        if(num == n){
+            return 1;
+        }
+    }
+    return 0;
+}
