60a81f7035b7e918e380d705331391127e3f93f7
송 필 수 <104068583+P1su@users.noreply.github.com>
2026-03-30 15:58:33 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 13.88 ms, Memory: 7.55 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(string myString) {
+    vector<string> answer;
+    string temp = "";
+    
+    for (auto c : myString) {
+        if (c == 'x') {
+            if (!temp.empty())
+                answer.push_back(temp);
+            temp = "";
+        } else {
+            temp += c;
+        }
+    }
+
+    if (!temp.empty())
+        answer.push_back(temp);
+    
+    sort(answer.begin(), answer.end());
+    return answer;
+}
